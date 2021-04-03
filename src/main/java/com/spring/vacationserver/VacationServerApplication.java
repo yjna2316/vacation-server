54384ac02fcc3860fc6aa358f2709452be881af8
Yoonji Na <yjna2316@naver.com>
2021-04-03 21:16:23 +0900

    first commit

---
+++ src/main/java/com/spring/vacationserver/VacationServerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.vacationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VacationServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/spring/vacationserver/VacationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.vacationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
